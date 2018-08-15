a3cf3fd25f977647acf5272f458f1e6820811939
Danny Moon <danny.moon@hanwha.com>
2018-08-15 18:43:47 -0400

    Improved Design

---
+++ js/test.js
@@ -0,0 +1,4 @@
+let person = {firstName: "John", lastName: "Doe", age: 46};
+for (var i = 0; i < person.length; i++) {
+	alert(person[i]);
+}
---
+++ sunapi.php
@@ -0,0 +1,22 @@
+<?php
+
+$sunapi = array(
+	"WDR" => array(
+		"WDR level" => array("Low", "Middle", "High"),
+		"Auto" => array("On", "Off"),
+		"Low Light" => array("On", "Off"),
+		"B/W" => array("On", "Off")
+	),
+
+	"HLC" => array(
+		"HLC level" => array("Low", "Middle", "High"),
+		"Mask" => array("Off", "On", "Night Only", "All Day"),
+		"Mask Color" => array("Black", "Blue", "Red", "Cyan", "Magenta"),
+
+	)
+
+);
+
+require("test.php");
+
+?>
