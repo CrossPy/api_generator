f243727440cd497e31c7aabbe30b090cab104a78
Danny <dmoon1112@gmail.com>
2018-09-06 21:39:19 -0400

    Made some Major breakthroughs with the IR tab. the UI with text area seems to come along nicely.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,79 @@
 $(document).ready(function() {
 
-	var previous_function;
-	var previous_parameter;
+	var function_value = "";
+	var textarea = "";
+	var modetext = "";
 	var currentID;
+	var parameter_value;
+	var values = "";
+	var handle = $("#custom-handle");
+	var slider_value;
 
 	$('textarea#sunapi_command').val('Welcome, Let\'s get started!');
+
+	//=================================================//
+	//
+	// Focus on IR first
+	//
+	//=================================================//
+
+	$(".box-gray div > .function").on("click", function() {
+		currentID = $(this).attr("data-target"); // id="ir"
+		textarea = "";
+		function_value = this.value;
+		switch(currentID) {
+			case "#ir":
+				$("textarea#sunapi_command").val(function_value);
+				$(`#setting > ${currentID} > .config [class$='_parameter']`).on("change", function() {
+					parameter_value = $(`${currentID} [class$='_parameter']`);
+					values = "";
+					for (i = 0; i < parameter_value.length; i++) {
+			    		values += parameter_value.eq(i).val();
+
+			    	}
+
+			    	$("textarea#sunapi_command").val(function_value + values);
+			    	console.log(values);
+			    	if (values == "&param_manual") {
+			    		$("#slider").show()
+			    		$(function() {
+					    	$( "#slider" ).slider({
+		      					create: function() {
+		        					console.log(handle.text( $( this ).slider( "value" )));
+		        					
+		      					},
+		      					slide: function( event, ui ) {
+		        					handle.text( ui.value );
+		        					slider_value = ui.value;
+		        					console.log(slider_value);
+		        					$("textarea#sunapi_command").val(function_value + values + "&param_value" + slider_value);
+      							}
+    						});
+			    		});
+			    	} else {
+			    		$("#slider").hide();
+			    	}
+				})
+				break;
+			case "#backlight":
+				
+				break;
+			default:
+			textarea = "";
+		}
+	});
+
+	
+
+
+
 	//=================================================//
 	//
 	// THIS MAKES A MODIFICATION TO THE TEXTAREA.
 	//
 	//=================================================//
 
-	$(".check").find("select").on("change", function() {
+	/*$(".check").find("select").on("change", function() {
 		var val = $(this).val();
 		console.log(val);
 		if(val == "blc") {
@@ -52,13 +114,13 @@ $(document).ready(function() {
 	//============================================================//
 
 
-	/*$(function () {
+	$(function () {
 		$(".dropdown-menu a").click(function() {
 			$(".menu:first-child").text($(this).text());
 			$(".menu:first-child").val($(this).text());
 		});
 	});
-	*/
+	
 
 	//=================================================//
 	//
@@ -108,7 +170,7 @@ $(document).ready(function() {
 	    });
 
 
-	    /*$(`${currentID} select` ).on("change", function() {
+	    $(`${currentID} select` ).on("change", function() {
 
    			$("textarea#sunapi_command").val(function_value + " " + )
 
@@ -117,9 +179,9 @@ $(document).ready(function() {
       		});
   			$( "textarea#sunapi_command" ).text( function_value + parameter_value );
   			console.log(function_value + parameter_value);
-		}).trigger( "change" );*/
-	   	//if ($(this).hasClass("active")) {
-   		/*$("[class$='_parameter']" ).on("change", function() {
+		}).trigger( "change" );
+	   	if ($(this).hasClass("active")) {
+   		$("[class$='_parameter']" ).on("change", function() {
    			console.log("I'm in");
       		parameter_value = "";
 
@@ -128,8 +190,8 @@ $(document).ready(function() {
       		});
   			$( "textarea#sunapi_command" ).text( function_value + parameter_value );
   			console.log(function_value + parameter_value);
-		}).trigger( "change" );*/
-		//}
-	});  	
+		}).trigger( "change" );
+		}
+	});	*/
 });
 
