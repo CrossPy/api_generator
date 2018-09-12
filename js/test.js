efd5d050f1030145d6deb4be6f56fba2a962a18c
Danny <dmoon1112@gmail.com>
2018-09-11 23:07:10 -0400

    Pushing to github

--- js/test.js
+++ js/test.js
@@ -1,11 +1,2 @@
-    function_value = "";
-    function_value += this.value;
-   
-    $( "select[class$='_parameter']" ).change(function() {
-      parameter_value = "";
-      $( "select option:selected" ).each(function() {
-        parameter_value += this.value;
-      });
-      $( "textarea#sunapi_command" ).text( function_value + parameter_value );
-    }).trigger( "change" );
-  });
+// TEST FUNCTION TO SEND HTTP REQUEST WOULD BE HERE.
+
